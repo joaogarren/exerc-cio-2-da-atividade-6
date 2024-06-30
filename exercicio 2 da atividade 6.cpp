d86da535790f61e8068388f8c8c853212d49ff6f
João Paulo Espirandel de Souza Mendes <jpespirandelsouzam@gmail.com>
2024-06-30 15:13:56 -0300

    Add files via upload

---
+++ exercicio 2 da atividade 6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int op;
+
+    cout << "1. Floresta Encantada" << endl;
+    cout << "2. Montanhas do Destino" << endl;
+   cin >> op;
+
+    switch(op)
+    {
+        case 1:
+            cout << "Parabens,siga em frente e cuidado"<<endl;
+            break;
+            
+        case 2:
+            cout << "Parabens,siga em frente e cuidado"<<endl;
+            break;
+        }
+        
+        return 0;
+}
